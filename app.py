71d55200d66aaa685f74319b994c1856fb1e6dc7
Akshay P <141031773+Achutechie@users.noreply.github.com>
2025-10-01 23:59:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,4 +150,8 @@ def logout():
 def about():
     return render_template('about.html')
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
